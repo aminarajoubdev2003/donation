c4d25d8e84a2aec730e1096678c24209524611eb
aminarajoubdev2003 <amina.rajoub03@gmail.com>
2026-06-08 15:16:45 +0300

    seventeen commit

--- app/Http/Resources/Donater_ShowResource.php
+++ app/Http/Resources/Donater_ShowResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Campaign;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -20,6 +21,7 @@ class Donater_ShowResource extends JsonResource
         'uuid' => $this->uuid,
         'last_donation' => $this->contribution_amount,
         'currency_type' => $this->currency_type,
+        'campaing' => CampaignResource::make(Campaign::findOrFail($this->campaign_id)),
         'date' => Carbon::parse($this->created_at)->format('d M Y'),
         'method' => ($this->donate_directly==1) ? 'تبرع' : 'تعهد ',
         'status' => $this->status,
